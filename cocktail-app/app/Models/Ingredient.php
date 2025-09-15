2900168cb27dec78332139855bfa97bffdc5c226
Rafa Bello <rafabp986@outlook.com>
2025-09-15 13:38:56 +0200

    ingredient model

--- cocktail-app/app/Models/Ingredient.php
+++ cocktail-app/app/Models/Ingredient.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
-{
-    //
+{   
+    use HasFactory;
+
+    protected $fillable = ["nombre", "tipo", "sabor"];
+
+    public function cocktails() {
+        return $this->belongsToMany(
+            Cocktail::class,
+            "cocktail_ingredient",
+            "id_ingrediente",
+            "id_cocktail")->withTimestamps();
+    }
 }
