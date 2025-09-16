a8ce80d7fbe1259de7834fd39c5fc1b330287a0d
Rafa Bello <rafabp986@outlook.com>
2025-09-16 10:17:34 +0200

    ingredient controller, index, create, store, edit

--- cocktail-app/app/Http/Controllers/CoktailController.php
+++ cocktail-app/app/Http/Controllers/CoktailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ingredient;
 use Illuminate\Http\Request;
 
 class CoktailController extends Controller
@@ -11,7 +12,7 @@ class CoktailController extends Controller
      */
     public function index()
     {
-        //
+        
     }
 
     /**
