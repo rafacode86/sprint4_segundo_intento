ab019aecd97b2d50926a21dea44ce63e396c44ab
Rafa Bello <rafabp986@outlook.com>
2025-11-11 12:06:08 +0100

    repair register bug

--- cocktail-app/app/Http/Controllers/Auth/RegisteredUserController.php
+++ cocktail-app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,6 +45,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('cocktail.index', absolute: false));
+        return redirect(route('cocktails.index', absolute: false));
     }
 }
